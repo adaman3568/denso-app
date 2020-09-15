0f8383713844cbfbd89ebb1952c3ce1e0f39e389
adaman3568 <adaman3568@gmail.com>
2020-09-15 21:10:48 +0900

    UserIndexにツイートリストを追加

--- src/Components/UserIndex/UserIndex.tsx
+++ src/Components/UserIndex/UserIndex.tsx
@@ -22,6 +22,7 @@ import { mainListItems } from './ListItem';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Order';
+import Tweets from "./Tweets";
 
 function Copyright() {
     return (
@@ -169,23 +170,8 @@ const UserIndex : React.FC = () => {
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
                     <Grid container spacing={3}>
-                        {/* Chart */}
-                        <Grid item xs={12} md={8} lg={9}>
-                            <Paper className={fixedHeightPaper}>
-                                <Chart />
-                            </Paper>
-                        </Grid>
-                        {/* Recent Deposits */}
-                        <Grid item xs={12} md={4} lg={3}>
-                            <Paper className={fixedHeightPaper}>
-                                <Deposits />
-                            </Paper>
-                        </Grid>
-                        {/* Recent Orders */}
                         <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <Orders />
-                            </Paper>
+                            <Tweets/>
                         </Grid>
                     </Grid>
                     <Box pt={4}>
