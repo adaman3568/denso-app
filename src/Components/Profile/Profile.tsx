9dd6cee918864bc9047c7f150ae5d33ec23eca1f
adaman3568 <adaman3568@gmail.com>
2020-09-17 22:49:45 +0900

    Profileページのたたき作成

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,10 +1,64 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+import {Button, Card, Grid, TextField, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const myStyle = makeStyles((theme) => ({
+    dummyImg :{
+        height : '150px',
+        width : '150px',
+        borderRadius : '50%',
+        backgroundColor : 'gray'
+    },
+    card : {
+        padding : theme.spacing(3)
+    },
+    center : {
+        margin : '0 auto'
+    },
+    flexCenter : {
+        display : 'flex',
+        justifyContent : 'center',
+        alignItems : 'center',
+    }
+
+}))
 
 const Profile : React.FC = () => {
+    const classes = myStyle();
+
+    const [isEdit,setIsEdit] = useState<boolean>(false)
+
     return (
-        <div>
-            <h1>this is profile page</h1>
-        </div>
+        <Card className={classes.card}>
+            <Grid container
+                  spacing={3}
+                  direction="row"
+                  justify="space-evenly"
+                  alignItems="center"
+            >
+                <Grid item xs={12} className={classes.flexCenter}>
+                    <div className={classes.dummyImg}>img</div>
+                </Grid>
+                <Grid item xs={12}
+                      direction="row"
+                className={classes.flexCenter}>
+                    {!isEdit ?
+                        <Typography onClick={() => setIsEdit(true)}>登録名 : 林山　浩</Typography> :
+                        <div className={classes.flexCenter}>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="登録名"
+                                defaultValue="林山　浩"
+                                variant="outlined"
+                            />
+                            <Button onClick={() => setIsEdit(false)}>登録</Button>
+                        </div>
+                    }
+
+                </Grid>
+            </Grid>
+        </Card>
     );
 };
 
