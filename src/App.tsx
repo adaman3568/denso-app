bbd08b686186a73048d9db2ac103a8cb7ef5cf53
adaman3568 <adaman3568@gmail.com>
2020-09-17 22:25:56 +0900

    Profile編集ページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter,Route,Switch} from "react-router-dom";
 import UserIndexWrapper from "./Components/UserIndex/UserIndexWrapper";
 import Tweets from "./Components/Tweets/Tweets";
 import {PathList} from "./Routing/path";
+import Profile from "./Components/Profile/Profile";
 
 const App : React.FC = () => {
     return (
@@ -11,6 +12,7 @@ const App : React.FC = () => {
             <Switch>
                 <UserIndexWrapper>
                     <Route exact path={PathList.home} component={Tweets}/>
+                    <Route exact path={PathList.profile} component={Profile}/>
                 </UserIndexWrapper>
             </Switch>
         </BrowserRouter>
---
+++ src/Components/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Profile : React.FC = () => {
+    return (
+        <div>
+            <h1>this is profile page</h1>
+        </div>
+    );
+};
+
+export default Profile;
