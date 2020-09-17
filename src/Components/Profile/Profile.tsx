2fca0cdb987b0e9e4fbe8ec3c0546317a756eded
adaman3568 <adaman3568@gmail.com>
2020-09-17 22:59:02 +0900

    Profileページに自分がつぶやいたTweetを表示

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useState} from 'react';
-import {Button, Card, Grid, TextField, Typography} from "@material-ui/core";
+import {Button, Card, Container, Grid, TextField, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import Tweets from "../Tweets/Tweets";
 
 const myStyle = makeStyles((theme) => ({
     dummyImg :{
@@ -10,7 +11,8 @@ const myStyle = makeStyles((theme) => ({
         backgroundColor : 'gray'
     },
     card : {
-        padding : theme.spacing(3)
+        padding : theme.spacing(3),
+        marginBottom : theme.spacing(5)
     },
     center : {
         margin : '0 auto'
@@ -29,36 +31,40 @@ const Profile : React.FC = () => {
     const [isEdit,setIsEdit] = useState<boolean>(false)
 
     return (
-        <Card className={classes.card}>
-            <Grid container
-                  spacing={3}
-                  direction="row"
-                  justify="space-evenly"
-                  alignItems="center"
-            >
-                <Grid item xs={12} className={classes.flexCenter}>
-                    <div className={classes.dummyImg}>img</div>
-                </Grid>
-                <Grid item xs={12}
+        <Container>
+            <Card className={classes.card}>
+                <Grid container
+                      spacing={3}
                       direction="row"
-                className={classes.flexCenter}>
-                    {!isEdit ?
-                        <Typography onClick={() => setIsEdit(true)}>登録名 : 林山　浩</Typography> :
-                        <div className={classes.flexCenter}>
-                            <TextField
-                                required
-                                id="outlined-required"
-                                label="登録名"
-                                defaultValue="林山　浩"
-                                variant="outlined"
-                            />
-                            <Button onClick={() => setIsEdit(false)}>登録</Button>
-                        </div>
-                    }
-
+                      justify="space-evenly"
+                      alignItems="center"
+                >
+                    <Grid item xs={12} className={classes.flexCenter}>
+                        <div className={classes.dummyImg}>img</div>
+                    </Grid>
+                    <Grid item xs={12}
+                          direction="row"
+                          className={classes.flexCenter}>
+                        {!isEdit ?
+                            <Typography onClick={() => setIsEdit(true)}>登録名 : 林山　浩</Typography> :
+                            <div className={classes.flexCenter}>
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="登録名"
+                                    defaultValue="林山　浩"
+                                    variant="outlined"
+                                />
+                                <Button onClick={() => setIsEdit(false)}>登録</Button>
+                            </div>
+                        }
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Card>
+            </Card>
+            <Tweets/>
+        </Container>
+
+
     );
 };
 
