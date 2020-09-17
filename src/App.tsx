42ce700c8d98d04896b99e97219490fa4d020e95
adaman3568 <adaman3568@gmail.com>
2020-09-17 21:47:59 +0900

    UserIndexWrapperを作成しルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ImageUploader from "./ImageUploader";
 import {BrowserRouter,Route,Switch} from "react-router-dom";
+import UserIndexWrapper from "./Components/UserIndex/UserIndexWrapper";
+import Tweets from "./Components/Tweets/Tweets";
 
 const App : React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact component={ImageUploader} path={'/'}/>
+                <UserIndexWrapper>
+                    <Route exact path={`/`} component={Tweets}/>
+                    <Route exact path={`/imguploader`} component={ImageUploader}/>
+                </UserIndexWrapper>
             </Switch>
         </BrowserRouter>
     );
