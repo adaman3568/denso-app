f409644b33e56f37b7dd23b9b7f7e7f44d3c0aa6
adaman3568 <adaman3568@gmail.com>
2020-09-26 19:35:57 +0900

    従業員一覧ページにデータを入れれるように

--- src/Components/Employee/EmployeeIndex.tsx
+++ src/Components/Employee/EmployeeIndex.tsx
@@ -1,11 +1,52 @@
 import React, {FC} from 'react';
+import {Card, Grid} from "@material-ui/core";
+
+type EmployeeInfo = {
+    id : number,
+    EmpName : string,
+    eMail : string,
+    CommentCount : number,
+    LastCommentDate : string
+}
+
+const EmpItems : EmployeeInfo[] = [
+    {id : 1, EmpName : '林山　浩',eMail : 'h.hayashiyama@co-rst.com', CommentCount : 10, LastCommentDate : '2020/12/10 10:10:23'},
+    {id : 2, EmpName : '池田　しんのすけ',eMail : 's.ikeda@co-rst.com', CommentCount : 12, LastCommentDate : '2020/12/8 09:08:20'},
+    {id : 3, EmpName : '浅田　和則',eMail : 'k.asada@co-rst.com', CommentCount : 1400, LastCommentDate : '2020/12/11 19:22:24'}
+]
 
 const EmployeeIndex : FC = () => {
     return (
         <div>
             <h2>This is Emp page.</h2>
+            {EmpItems.map(emp => <EmployeeItem key={emp.id} Emp={emp}/>)}
         </div>
     );
 };
+
+type Props = {
+    Emp : EmployeeInfo
+}
+
+const EmployeeItem : FC<Props> = ({Emp}) => {
+    return (
+        <Card>
+            <Grid container>
+                <Grid sm={12}>
+                    {Emp.EmpName}
+                </Grid>
+                <Grid sm={12}>
+                    {Emp.eMail}
+                </Grid>
+                <Grid sm={6}>
+                    コメント件数：{Emp.CommentCount}
+                </Grid>
+                <Grid sm={6}>
+                    最終コメント日付：{Emp.LastCommentDate}
+                </Grid>
+            </Grid>
+        </Card>
+    )
+}
 
 export default EmployeeIndex;
