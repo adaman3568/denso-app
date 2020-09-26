0250f8d4a94609c0d5cddb298bf5a808fa99cef5
adaman3568 <adaman3568@gmail.com>
2020-09-26 19:47:16 +0900

    車両一覧ページの叩き作成

--- src/Components/Car/CarIndex.tsx
+++ src/Components/Car/CarIndex.tsx
@@ -1,11 +1,42 @@
 import React, {FC} from 'react';
+import {Card, Grid} from "@material-ui/core";
+
+type CarInfo = {
+    id : number,
+    CarName : string,
+    Detail : string,
+    CommentCount : number,
+    LastCommentDate : string
+}
+
+const CarItems : CarInfo[] = [
+    {id : 1,CarName : '11-22',Detail : 'etc装着済み',CommentCount : 12 , LastCommentDate : '2020/12/11 12:34:23'},
+    {id : 1,CarName : '12-45',Detail : 'etc装着済み＆デジタコ',CommentCount : 10 , LastCommentDate : '2020/12/10 10:45:11'}
+]
 
 const CarIndex : FC = () => {
     return (
         <div>
             <h2>This is CarIndex page.</h2>
+            {CarItems.map(car => <CarItem key={car.id} Car={car} />)}
         </div>
     );
 };
 
+type Props = {
+    Car : CarInfo
+}
+
+const CarItem : FC<Props> = ({Car}) => {
+    return (
+        <Card>
+            <Grid container>
+                <Grid sm={12}>{Car.CarName}</Grid>
+                <Grid sm={12}>{Car.Detail}</Grid>
+                <Grid sm={6}>コメント件数{Car.CommentCount}</Grid>
+                <Grid sm={6}>最終コメント日時：{Car.LastCommentDate}</Grid>
+            </Grid>
+        </Card>
+    )
+}
 export default CarIndex;
