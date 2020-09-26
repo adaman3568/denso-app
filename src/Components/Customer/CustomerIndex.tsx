a6fe775a3a735eea60c16712ce0e447fca94f58d
adaman3568 <adaman3568@gmail.com>
2020-09-26 09:13:15 +0900

    一旦コミット

--- src/Components/Customer/CustomerIndex.tsx
+++ src/Components/Customer/CustomerIndex.tsx
@@ -1,4 +1,18 @@
 import React, {FC} from 'react';
+import {Card, Grid} from "@material-ui/core";
+
+type CustomerInfo = {
+    id : number
+    customerName : string
+    CarCount : number
+    CommentCount : number
+    LastCommentDate : string
+    Address : string
+}
+
+const CustomerInfoList : CustomerInfo[] = [
+    {id : 1,customerName : "合同会社Rst.com",CarCount : 1,CommentCount : 2,LastCommentDate : '2020/09/21',Address : "東京都世田谷区1-2-3"}
+]
 
 const CustomerIndex : FC = () => {
     return (
@@ -7,5 +21,23 @@ const CustomerIndex : FC = () => {
         </div>
     );
 };
+
+
+type Props = {
+    Customer : CustomerInfo
+}
+
+
+
+
+const CustomerItem : FC<Props> = ({Customer}) => {
+    return(
+        <Card>
+            <Grid container>
+
+            </Grid>
+        </Card>
+    )
+}
 
 export default CustomerIndex;
