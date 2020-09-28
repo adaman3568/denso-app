5402e03ac6d7844e6402b96d3ff41d552467e73c
adaman3568 <adaman3568@gmail.com>
2020-09-28 23:17:48 +0900

    従業員コンポーネントを分けた

--- src/Components/Employee/EmployeeIndex.tsx
+++ src/Components/Employee/EmployeeIndex.tsx
@@ -2,8 +2,9 @@ import React, {FC} from 'react';
 import {Card, Grid, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import CardTitle from "../Common/CardTitle";
+import EmployeeItem from "./EmpItem";
 
-type EmployeeInfo = {
+export type EmployeeInfo = {
     id : number,
     EmpName : string,
     eMail : string,
@@ -26,47 +27,6 @@ const EmployeeIndex : FC = () => {
     );
 };
 
-const myStyle = makeStyles((theme) => ({
-    card : {
-        margin : theme.spacing(3),
-        padding : theme.spacing(2)
-    },
-    empName : {
-        padding : theme.spacing(2)
-    },
-    eMail : {
-        padding : theme.spacing(2)
-    }
-}))
 
-type Props = {
-    Emp : EmployeeInfo
-}
-
-const EmployeeItem : FC<Props> = ({Emp}) => {
-    const classes = myStyle();
-    return (
-        <Card className={classes.card}>
-            <Grid container>
-                <Grid sm={12}>
-                    <CardTitle>
-                        {Emp.EmpName}
-                    </CardTitle>
-                </Grid>
-                <Grid sm={12}>
-                    <Typography variant={'h5'} className={classes.eMail}>
-                        {Emp.eMail}
-                    </Typography>
-                </Grid>
-                <Grid sm={6}>
-                    コメント件数：{Emp.CommentCount}
-                </Grid>
-                <Grid sm={6}>
-                    最終コメント日付：{Emp.LastCommentDate}
-                </Grid>
-            </Grid>
-        </Card>
-    )
-}
 
 export default EmployeeIndex;
