6a491307f975070e28c34c22083e8fcd3dd0d4f3
adaman3568 <adaman3568@gmail.com>
2020-09-29 21:18:02 +0900

    Titleコンポーネントのスタイルを簡単に入れた

--- src/Components/Common/Title.tsx
+++ src/Components/Common/Title.tsx
@@ -1,8 +1,20 @@
 import React, {FC} from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+
+const myStyle = makeStyles((theme) => ({
+    title: {
+        borderBottom: '1px solid',
+        borderColor: theme.palette.primary.main,
+        paddingBottom : theme.spacing(1),
+        paddingLeft : theme.spacing(2),
+        marginBottom : theme.spacing(2)
+    }
+}))
 
 const Title : FC = ({children}) => {
+    const classes = myStyle();
     return (
-        <h2>{children}</h2>
+        <h2 className={classes.title}>{children}</h2>
     );
 };
 
