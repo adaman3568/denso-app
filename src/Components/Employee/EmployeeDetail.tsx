08b9e7c11fe3bcd6767f002b6719e1c4f0edc782
adaman3568 <adaman3568@gmail.com>
2020-09-30 22:47:29 +0900

    EmpDetailでデータを取得できるように

--- src/Components/Employee/EmployeeDetail.tsx
+++ src/Components/Employee/EmployeeDetail.tsx
@@ -1,12 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useContext, useEffect, useState} from 'react';
 import {RouteComponentProps} from "react-router-dom";
+import {DataContext} from "../../Context/DataContextProvider";
+import {EmployeeInfo} from "../../Context/DataTypeList";
 
 type pageProps = {} & RouteComponentProps<{id : string}>
 
 const EmployeeDetail : FC<pageProps> = ({match}) => {
+
+    const {EmpFunc} = useContext(DataContext)
+    const [emp,setEmp] = useState<EmployeeInfo>({} as EmployeeInfo);
+    useEffect(() => {
+        const d = EmpFunc.GetEmpData(parseInt(match.params.id))
+        if(d !== undefined)setEmp(d);
+    },[])
     return (
         <div>
             <h2>this is id:{match.params.id}'s emp page.</h2>
+            <p>{emp.id}</p>
+            <p>{emp.CommentCount}</p>
+            <p>{emp.eMail}</p>
+            <p>{emp.EmpName}</p>
+            <p>{emp.LastCommentDate}</p>
         </div>
     );
 };
