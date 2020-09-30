4d763793203c014e16ce84208fcf0fc528721e51
adaman3568 <adaman3568@gmail.com>
2020-09-30 21:53:43 +0900

    CarItemにLinkCardを入れた

--- src/Components/Car/CarItem.tsx
+++ src/Components/Car/CarItem.tsx
@@ -6,48 +6,39 @@ import CommentCount from "../Common/CommentCount";
 import {CarInfo} from "./CarIndex";
 import {Link} from "react-router-dom";
 import {PathList} from "../../Routing/path";
+import LinkCard from "../Common/LinkCard";
 
 type Props = {
     Car : CarInfo
 }
 
 const myStyle = makeStyles((theme) => ({
-    card : {
-        margin : theme.spacing(3),
-        padding : theme.spacing(2)
-    },
     detail : {
         padding : theme.spacing(2)
     },
-    CardLink : {
-        textDecoration : 'none'
-    }
 }));
 
 const CarItem : FC<Props> = ({Car}) => {
     const classes = myStyle();
     return (
-        <Link to={`${PathList.carDetail}${Car.id}`} className={classes.CardLink}>
-            <Card className={classes.card}>
-                <Grid container>
-                    <Grid sm={12}>
-                        <CardTitle>
-                            {Car.CarName}
-                        </CardTitle>
-                    </Grid>
-                    <Grid sm={12}>
-                        <Typography className={classes.detail}>
-                            {Car.Detail}
-                        </Typography>
-                    </Grid>
-                    <Grid sm={6}>
-                        <CommentCount displayCount={Car.CommentCount}/>
-                    </Grid>
-                    <Grid sm={6}>最終コメント日時：{Car.LastCommentDate}</Grid>
+        <LinkCard path={`${PathList.carDetail}${Car.id}`}>
+            <Grid container>
+                <Grid sm={12}>
+                    <CardTitle>
+                        {Car.CarName}
+                    </CardTitle>
                 </Grid>
-            </Card>
-        </Link>
-
+                <Grid sm={12}>
+                    <Typography className={classes.detail}>
+                        {Car.Detail}
+                    </Typography>
+                </Grid>
+                <Grid sm={6}>
+                    <CommentCount displayCount={Car.CommentCount}/>
+                </Grid>
+                <Grid sm={6}>最終コメント日時：{Car.LastCommentDate}</Grid>
+            </Grid>
+        </LinkCard>
     )
 };
 
