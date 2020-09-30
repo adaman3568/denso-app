86e32f7566de9f589c821fcbadeafd0b3ceb1f82
adaman3568 <adaman3568@gmail.com>
2020-09-30 22:24:03 +0900

    DataContextProvider全体に反映

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,29 @@ import TweetsDetail from "./Components/Tweets/TweetsDetail";
 import CarDetail from "./Components/Car/CarDetail";
 import CustomerDetail from "./Components/Customer/CustomerDetail";
 import EmployeeDetail from "./Components/Employee/EmployeeDetail";
+import DataContextProvider from "./Context/DataContextProvider";
 
 const App : React.FC = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <UserIndexWrapper>
-                    <Route exact path={PathList.home} component={Tweets}/>
-                    <Route exact path={PathList.profile} component={ProfileIndex}/>
-                    <Route exact path={PathList.employee} component={EmployeeIndex}/>
-                    <Route exact path={`${PathList.employeeDetail}/:id`} component={EmployeeDetail}/>
+            <DataContextProvider>
+                <Switch>
+                    <UserIndexWrapper>
+                        <Route exact path={PathList.home} component={Tweets}/>
+                        <Route exact path={PathList.profile} component={ProfileIndex}/>
+                        <Route exact path={PathList.employee} component={EmployeeIndex}/>
+                        <Route exact path={`${PathList.employeeDetail}/:id`} component={EmployeeDetail}/>
 
-                    <Route exact path={PathList.cars} component={CarIndex}/>
-                    <Route exact path={`${PathList.carDetail}/:id`} component={CarDetail}/>
+                        <Route exact path={PathList.cars} component={CarIndex}/>
+                        <Route exact path={`${PathList.carDetail}/:id`} component={CarDetail}/>
 
-                    <Route exact path={PathList.customers} component={CustomerIndex}/>
-                    <Route exact path={`${PathList.customerDetail}/:id`} component={CustomerDetail}/>
+                        <Route exact path={PathList.customers} component={CustomerIndex}/>
+                        <Route exact path={`${PathList.customerDetail}/:id`} component={CustomerDetail}/>
 
-                    <Route exact path={PathList.tweetsDetail} component={TweetsDetail}/>
-                </UserIndexWrapper>
-            </Switch>
+                        <Route exact path={PathList.tweetsDetail} component={TweetsDetail}/>
+                    </UserIndexWrapper>
+                </Switch>
+            </DataContextProvider>
         </BrowserRouter>
     );
 };
