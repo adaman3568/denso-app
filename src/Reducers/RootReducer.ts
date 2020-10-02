a0e5853d8ae6767e82d8c1b9300f6527c2dd931e
adaman3568 <adaman3568@gmail.com>
2020-10-02 12:47:22 +0900

    車輛IDを振りなおした

--- src/Reducers/RootReducer.ts
+++ src/Reducers/RootReducer.ts
@@ -17,12 +17,12 @@ const CustomerInitialState : CustomerInfo[] = [
     {id : 1,customerName : "合同会社Rst.com",CarCount : 1,CommentCount : 2,LastCommentDate : '2020/09/21 12:45:22' ,Address : "東京都世田谷区1-2-3", Cars :
             [
                 {id : 1,CarName : '11-22',Detail : 'etc装着済み',CommentCount : 12 , LastCommentDate : '2020/12/11 12:34:23'},
-                {id : 1,CarName : '12-45',Detail : 'etc装着済み＆デジタコ',CommentCount : 10 , LastCommentDate : '2020/12/10 10:45:11'}
+                {id : 2,CarName : '12-45',Detail : 'etc装着済み＆デジタコ',CommentCount : 10 , LastCommentDate : '2020/12/10 10:45:11'}
             ]},
     {id : 2,customerName : "株式会社タイガー",CarCount : 5,CommentCount : 102,LastCommentDate : '2020/09/22 22:23:45',Address : "東京都世田谷区1-2-3" , Cars:
             [
-                {id : 1,CarName : '11-22',Detail : 'etc装着済み',CommentCount : 12 , LastCommentDate : '2020/12/11 12:34:23'},
-                {id : 1,CarName : '12-45',Detail : 'etc装着済み＆デジタコ',CommentCount : 10 , LastCommentDate : '2020/12/10 10:45:11'}
+                {id : 3,CarName : '11-22',Detail : 'etc装着済み',CommentCount : 12 , LastCommentDate : '2020/12/11 12:34:23'},
+                {id : 4,CarName : '12-45',Detail : 'etc装着済み＆デジタコ',CommentCount : 10 , LastCommentDate : '2020/12/10 10:45:11'}
             ]}
 ];
 
