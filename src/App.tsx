755667eba04869e15a8f9617a4cd80e203a706a4
adaman3568 <adaman3568@gmail.com>
2020-10-02 23:32:01 +0900

    Muiのthemeを注入できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,48 @@ import EmployeeDetail from "./Components/Employee/EmployeeDetail";
 import DataContextProvider from "./Context/DataContextProvider";
 import CarIndex from "./Components/Car/CarIndex";
 import CarDetail from "./Components/Car/CarDetail";
+import {createMuiTheme} from "@material-ui/core";
+import {pink, teal} from "@material-ui/core/colors";
+import {MuiThemeProvider} from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette :{
+        primary : {
+            dark : teal[700],
+            main : teal[500],
+            light : teal[300]
+        },
+        secondary :{
+            dark : '#ffa000',
+            main : '#ffc107',
+            light : '#ffd54f'
+        }
+    }
+});
 
 const App : React.FC = () => {
     return (
         <BrowserRouter>
             <DataContextProvider>
-                <Switch>
-                    <UserIndexWrapper>
-                        <Route exact path={PathList.home} component={Tweets}/>
-                        <Route exact path={PathList.profile} component={ProfileIndex}/>
-                        <Route exact path={PathList.employee} component={EmployeeIndex}/>
-                        <Route exact path={`${PathList.employeeDetail}/:id`} component={EmployeeDetail}/>
+                <MuiThemeProvider theme={theme}>
+                    <Switch>
+                        <UserIndexWrapper>
+                            <Route exact path={PathList.home} component={Tweets}/>
+                            <Route exact path={PathList.profile} component={ProfileIndex}/>
+                            <Route exact path={PathList.employee} component={EmployeeIndex}/>
+                            <Route exact path={`${PathList.employeeDetail}/:id`} component={EmployeeDetail}/>
 
-                        <Route exact path={PathList.cars} component={CarIndex}/>
-                        <Route exact path={`${PathList.carDetail}/:id`} component={CarDetail}/>
+                            <Route exact path={PathList.cars} component={CarIndex}/>
+                            <Route exact path={`${PathList.carDetail}/:id`} component={CarDetail}/>
 
-                        <Route exact path={PathList.customers} component={CustomerIndex}/>
-                        <Route exact path={`${PathList.customerDetail}/:id`} component={CustomerDetail}/>
+                            <Route exact path={PathList.customers} component={CustomerIndex}/>
+                            <Route exact path={`${PathList.customerDetail}/:id`} component={CustomerDetail}/>
 
-                        <Route exact path={PathList.tweetsDetail} component={TweetsDetail}/>
+                            <Route exact path={PathList.tweetsDetail} component={TweetsDetail}/>
 
-                    </UserIndexWrapper>
-                </Switch>
+                        </UserIndexWrapper>
+                    </Switch>
+                </MuiThemeProvider>
             </DataContextProvider>
         </BrowserRouter>
     );
