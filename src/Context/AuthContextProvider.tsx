941ad71cc5c3b8e13bf3837e1bf70c1151bc3c2f
adaman3568 <adaman3568@gmail.com>
2020-10-04 21:01:23 +0900

    AuthProviderにAuthReducerを導入

--- src/Context/AuthContextProvider.tsx
+++ src/Context/AuthContextProvider.tsx
@@ -1,16 +1,22 @@
-import React, {createContext, FC} from 'react';
+import React, {createContext, FC, useReducer} from 'react';
+import AuthReducer, {AuthActions, AuthReducerStateType} from "../Reducers/AuthReducer";
+import {type} from "os";
 
 export const AuthContext = createContext<IAuthContextState>({} as IAuthContextState)
 
 export interface IAuthContextState {
     isLogin : boolean
+    dispatch : (action : AuthActions) => void
 }
 
-
 const AuthContextProvider : FC = ({children}) => {
+
+    const [state, dispatch] = useReducer<typeof AuthReducer>(AuthReducer,{isLogin : false})
+
     return (
         <AuthContext.Provider value={
-            {isLogin : false}
+            {isLogin : false,
+            dispatch}
         }>
             {children}
         </AuthContext.Provider>
