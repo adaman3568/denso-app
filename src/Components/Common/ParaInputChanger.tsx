f1da175f567b058f76a00800a9ed9a187baeb92b
adaman3568 <adaman3568@gmail.com>
2020-10-11 17:33:32 +0900

    車両編集テキストボックスのスタイルを少し整えた

--- src/Components/Common/ParaInputChanger.tsx
+++ src/Components/Common/ParaInputChanger.tsx
@@ -1,5 +1,6 @@
 import React, {FC, useState} from 'react';
 import {Button, TextField, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 type Props = {
     value : string,
@@ -7,23 +8,38 @@ type Props = {
     setValueFunc : (value : string) => void
 }
 
+const useStyle = makeStyles((theme) => ({
+    margin : {
+        marginTop : theme.spacing(2)
+    }
+}));
+
 const ParaInputChanger : FC<Props> = ({value,TextBoxName,setValueFunc}) => {
 
     const [isInput ,setIsInput] = useState<boolean>(false);
 
-    return (
-        <div>
-            {isInput ?
-                <div>
+    const classes = useStyle();
+
+    const Input = () => {
+        return (
+            <>
                 <TextField
-                id="outlined-basic"
-                label={TextBoxName}
-                variant="outlined"
-                value={value}
-                onChange={(e) => setValueFunc(e.target.value)}/>
+                    id="outlined-basic"
+                    label={TextBoxName}
+                    variant="outlined"
+                    value={value}
+                    onChange={(e) => setValueFunc(e.target.value)}/>
                 <Button onClick={() => setIsInput(false)}>更新</Button>
-                </div>:
-                <Typography onClick={() => setIsInput(true)}>{TextBoxName} : {value}</Typography>}
+            </>
+        )
+    };
+    const Text = () => {
+        return(<Typography onClick={() => setIsInput(true)}>{TextBoxName} : {value}</Typography>)
+    };
+
+    return (
+        <div className={classes.margin}>
+            {isInput ? <Input/> :  <Text/>}
         </div>
     );
 };
