4b3e794be6b9149ae74195d732d4a541e5b6e39f
adaman3568 <adaman3568@gmail.com>
2020-10-16 12:12:17 +0900

    とりあえずトップページにTabメニューを追加

--- src/Components/Tweets/Tweets.tsx
+++ src/Components/Tweets/Tweets.tsx
@@ -2,6 +2,7 @@ import React, {useContext, useEffect, useState} from 'react';
 import Tweet from "./Tweet";
 import {CommentInfo} from "../../Context/DataTypeList";
 import {DataContext} from "../../Context/DataContextProvider";
+import ScrollableTabsButtonAuto from "../Common/Tab/TabPanel";
 
 type Props = {
     ShowImg? : boolean
@@ -11,6 +12,7 @@ const Tweets : React.FC<Props> = ({ShowImg = true}) => {
     const {Comment} = useContext(DataContext);
     return (
         <div>
+            <ScrollableTabsButtonAuto/>
             <h2>Tweets</h2>
             {Comment.Data.map((item,index) => <Tweet key={index} tweet={item} ShowImg={ShowImg}/>)}
         </div>
