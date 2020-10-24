9d7051d2b4002b264b74d6d708077f57c8ebfdd7
adaman3568 <adaman3568@gmail.com>
2020-10-25 00:04:05 +0900

    AddCustomerをModal化

--- src/Components/Customer/CustomerIndex.tsx
+++ src/Components/Customer/CustomerIndex.tsx
@@ -1,18 +1,24 @@
-import React, {FC, useContext} from 'react';
+import React, {FC, useContext, useState} from 'react';
 import CustomerItem from "./CustomerItem";
 import Title from "../Common/Title";
 import {DataContext} from "../../Context/DataContextProvider";
 import LinkButton from "../Common/LinkButton";
 import {PathList} from "../../Routing/path";
+import ModalWindow from "../Common/ModalWindow";
+import AddCustomer from "./AddCustomer";
+import {Button} from "@material-ui/core";
 
 const CustomerIndex : FC = () => {
+
+    const [open,setOpen] = useState<boolean>(false);
 
     const {Customer} = useContext(DataContext);
     return (
         <div>
             <Title>this is CustomerIndex page.</Title>
-            <LinkButton path={PathList.customerCreate} value={'顧客追加'}/>
+            <Button size={'small'} color={"primary"} variant="contained" onClick={() => setOpen(true)}>顧客追加</Button>
             {Customer.Data.map((cu,index) => <CustomerItem key={index} Customer={cu}/> )}
+            <ModalWindow IsOpen={open} handleClose={() => setOpen(false)} ChildComponent={<AddCustomer/>}/>
         </div>
     );
 };
