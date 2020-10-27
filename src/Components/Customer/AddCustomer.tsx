ecede9661d9b662cc549606463b44674ff84fa39
adaman3568 <adaman3568@gmail.com>
2020-10-27 22:04:19 +0900

    顧客追加Modalを作成

--- src/Components/Customer/AddCustomer.tsx
+++ src/Components/Customer/AddCustomer.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-// todo 顧客追加ページをつくる
+import {TextField,Button,Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyle = makeStyles((theme) => ({
+    btnCenter : {
+        display : 'flex',
+        justifyContent : 'center',
+        marginTop : theme.spacing(2)
+    },
+    submitBtn : {
+        width : '80%'
+    },
+    tokuisakiTextField : {
+        width : '90%'
+    },
+    addWindow : {
+        width : '500px'
+    }
+}))
+
 const AddCustomer = () => {
+    const classes = useStyle();
+
     return (
         <div>
-            <h2>Add Customer</h2>
+            <Grid container className={classes.addWindow}>
+                <Grid item xs={12} className={classes.btnCenter}>
+                    <TextField label={'得意先名'} className={classes.tokuisakiTextField}/>
+                </Grid>
+                <Grid item xs={12} className={classes.btnCenter}>
+                    <TextField label={'住所'} className={classes.tokuisakiTextField}/>
+                </Grid>
+                <Grid item xs={12} className={classes.btnCenter}>
+                    <Button className={classes.submitBtn} variant={'contained'} color={'primary'}>登録</Button>
+                </Grid>
+            </Grid>
         </div>
     );
 };
