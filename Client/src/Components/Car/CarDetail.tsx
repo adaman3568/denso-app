4738de8b2a0610186f17c341f1df9e469dea3d76
adaman3568 <adaman3568@gmail.com>
2020-11-23 20:02:21 +0900

    車両詳細の表示ができるように

--- Client/src/Components/Car/CarDetail.tsx
+++ Client/src/Components/Car/CarDetail.tsx
@@ -3,12 +3,36 @@ import {RouteComponentProps} from 'react-router-dom'
 import {CarInfo, CommentInfo} from "../../Context/DataTypeList";
 import Tweet from "../Tweets/Tweet";
 import CarProfile from "./CarProfile";
+import axios from "axios";
+import {apiEndPointBase} from "../../Firebase";
+import Cookies from "js-cookie";
 
 type CarPageProps = {} & RouteComponentProps<{id : string}>
 
 const CarDetail : FC<CarPageProps> = (props : CarPageProps) => {
     const [carData, setCarData] = useState<CarInfo>({} as CarInfo);
     const [comments, setComments] = useState<CommentInfo[]>([]);
+
+    useEffect(() => {
+        const token = Cookies.get("denso-app-jwt-token");
+        axios.get(`${apiEndPointBase}cars/${props.match.params.id}`,{headers :
+                {'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${token}`
+                }}).then(res => {
+                const d = res.data as CarInfo;
+                setCarData(d)
+            }
+        );
+
+        axios.get(`${apiEndPointBase}cars/${props.match.params.id}/comments`,{headers :
+                {'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${token}`
+                }}).then(res => {
+                const d = res.data as CommentInfo[];
+                setComments(d)
+            }
+        );
+    })
 
     return (
         <div>
