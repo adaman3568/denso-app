787b5a2316d3e3b017eaf94267bd97f6875df65e
adaman3568 <adaman3568@gmail.com>
2020-11-23 20:00:18 +0900

    車両一覧の表示ができるように

--- Client/src/Components/Car/CarIndex.tsx
+++ Client/src/Components/Car/CarIndex.tsx
@@ -1,18 +1,33 @@
 import React, {FC, useContext, useEffect, useState} from 'react';
 import Title from "../Common/Title";
 import CarItem from "./CarItem";
-import {CarInfo} from "../../Context/DataTypeList";
+import {CarInfo, CommentInfo} from "../../Context/DataTypeList";
 import ModalWindow from "../Common/ModalWindow";
 import useEditModal from "../../CustomHooks/useEditModal";
 import {CarEdit} from "./CarCreateEdit";
 import useDeleteModal from "../../CustomHooks/useDeleteModal";
 import {DeleteCar} from "./CarDelete";
+import Cookies from "js-cookie";
+import axios from "axios";
+import {apiEndPointBase} from "../../Firebase";
 
 const CarIndex : FC = () => {
     const carEditModal = useEditModal<CarInfo>(CarEdit);
     const carDeleteModal = useDeleteModal<CarInfo>(DeleteCar)
 
     const [carItems,setCarItems] = useState<CarInfo[]>([]);
+
+    useEffect(() => {
+        const token = Cookies.get("denso-app-jwt-token");
+        axios.get(`${apiEndPointBase}cars`,{headers :
+                {'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${token}`
+                }}).then(res => {
+                const d = res.data as CarInfo[];
+                setCarItems(d)
+            }
+        );
+    })
 
     return (
         <div>
