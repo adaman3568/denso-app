2783290fb00fa89c95a6a22c170e8d4807968638
adaman3568 <adaman3568@gmail.com>
2020-11-24 22:41:26 +0900

    resolve #15 車輌の削除を実装

--- Client/src/Components/Car/CarDelete.tsx
+++ Client/src/Components/Car/CarDelete.tsx
@@ -1,7 +1,10 @@
-import {CarInfo} from "../../Context/DataTypeList";
+import {CarInfo, CommentInfo} from "../../Context/DataTypeList";
 import React, {FC} from "react";
 import {Button, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import axios from "axios";
+import {apiEndPointBase} from "../../Firebase";
+import Cookies from "js-cookie";
 
 type props = {
     Car : CarInfo,
@@ -17,6 +20,22 @@ const useStyle = makeStyles((theme) => ({
 const CarDelete : FC<props> = ({Car,deleteConfirmOpen}) => {
     const classes = useStyle();
 
+    const deleteCar = () => {
+        const token = Cookies.get("denso-app-jwt-token");
+        axios.delete(`${apiEndPointBase}cars/${Car.id}`,{headers :
+                {'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${token}`
+                }}).then(res => {
+                    alert("車両を削除しました。")
+                    console.log(res)
+            }
+        ).catch(err => {
+            alert("車両を削除しました。")
+            console.log(err)
+        });
+        deleteConfirmOpen();
+    }
+
     return (
         <div className={classes.textCenter}>
             <div className={classes.textCenter}>
@@ -26,10 +45,11 @@ const CarDelete : FC<props> = ({Car,deleteConfirmOpen}) => {
                 <Typography display={'inline'} variant={'h3'}>詳細：{Car.detail}</Typography>
             </div>
             <div className={classes.textCenter}>
-                上記車両を本当に削除しますか？
+                上記車両を本当に削除しますか？<br/>
+                付随するコメントも全て削除されます。
             </div>
             <div className={classes.textCenter}>
-                <Button variant={'contained'} color={'primary'} onClick={deleteConfirmOpen}>削除</Button>
+                <Button variant={'contained'} color={'primary'} onClick={deleteCar}>削除</Button>
             </div>
         </div>
     )
