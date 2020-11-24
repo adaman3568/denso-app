383c2f4cf4d2759131ea6221a5281ad710b618f9
adaman3568 <adaman3568@gmail.com>
2020-11-24 22:44:18 +0900

    resolve #14 顧客の削除機能を実装

--- Client/src/Components/Customer/ComfirmDeleteCustomer.tsx
+++ Client/src/Components/Customer/ComfirmDeleteCustomer.tsx
@@ -2,6 +2,9 @@ import {CustomerInfo} from "../../Context/DataTypeList";
 import React, {FC} from "react";
 import {Button, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import Cookies from "js-cookie";
+import axios from "axios";
+import {apiEndPointBase} from "../../Firebase";
 
 type props = {
     cus : CustomerInfo,
@@ -17,6 +20,22 @@ const useStyle = makeStyles((theme) => ({
 const DeleteConfirmCustomer : FC<props> = ({cus,deleteConfirmOpen}) => {
     const classes = useStyle();
 
+    const customerDelete = () => {
+        const token = Cookies.get("denso-app-jwt-token");
+        axios.delete(`${apiEndPointBase}customers/${cus.id}`,{headers :
+                {'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${token}`
+                }}).then(res => {
+                alert("顧客を削除しました。")
+                console.log(res)
+            }
+        ).catch(err => {
+            alert("顧客を削除しました。")
+            console.log(err)
+        });
+        deleteConfirmOpen();
+    }
+
     return (
         <div className={classes.textCenter}>
             <div className={classes.textCenter}>
@@ -26,10 +45,11 @@ const DeleteConfirmCustomer : FC<props> = ({cus,deleteConfirmOpen}) => {
                 <Typography display={'inline'} variant={'h3'}>住所：{cus.address}</Typography>
             </div>
             <div className={classes.textCenter}>
-                上記顧客を本当に削除しますか？
+                上記顧客を本当に削除しますか？<br/>
+                付随する車輌及びコメントも全て削除されます。
             </div>
             <div className={classes.textCenter}>
-                <Button variant={'contained'} color={'primary'} onClick={deleteConfirmOpen}>削除</Button>
+                <Button variant={'contained'} color={'primary'} onClick={customerDelete}>削除</Button>
             </div>
         </div>
     )
