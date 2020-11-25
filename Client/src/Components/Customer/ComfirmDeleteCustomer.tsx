ce1777887611a220fc0b6eb3d36b8db80f322b6b
adaman3568 <adaman3568@gmail.com>
2020-11-25 22:51:46 +0900

    resolve #23 顧客削除画面の実装

--- Client/src/Components/Customer/ComfirmDeleteCustomer.tsx
+++ Client/src/Components/Customer/ComfirmDeleteCustomer.tsx
@@ -1,10 +1,11 @@
 import {CustomerInfo} from "../../Context/DataTypeList";
 import React, {FC} from "react";
-import {Button, Typography} from "@material-ui/core";
+import {Button, Grid, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import Cookies from "js-cookie";
 import axios from "axios";
 import {apiEndPointBase} from "../../Firebase";
+import CenteringGridItem from "../Common/CenteringGridItem";
 
 type props = {
     cus : CustomerInfo,
@@ -35,19 +36,24 @@ const DeleteConfirmCustomer : FC<props> = ({cus,successOpen,failedOpen}) => {
 
     return (
         <div className={classes.textCenter}>
-            <div className={classes.textCenter}>
-                <Typography display={'inline'} variant={'h3'}>顧客名：{cus.name}</Typography>
-            </div>
-            <div className={classes.textCenter}>
-                <Typography display={'inline'} variant={'h3'}>住所：{cus.address}</Typography>
-            </div>
-            <div className={classes.textCenter}>
-                上記顧客を本当に削除しますか？<br/>
-                付随する車輌及びコメントも全て削除されます。
-            </div>
-            <div className={classes.textCenter}>
-                <Button variant={'contained'} color={'primary'} onClick={customerDelete}>削除</Button>
-            </div>
+            <CenteringGridItem xs={12}>
+                <Typography display={'inline'} variant={'h4'}>削除確認</Typography>
+            </CenteringGridItem>
+            <CenteringGridItem xs={12}>
+                <Typography display={'inline'} variant={'body1'}>
+                    上記顧客を本当に削除しますか？<br/>
+                    付随する車輌及びコメントも全て削除されます。
+                </Typography>
+            </CenteringGridItem>
+            <CenteringGridItem xs={12}>
+                <Typography display={'inline'} variant={'body1'}>顧客名：{cus.name}</Typography>
+            </CenteringGridItem>
+            <CenteringGridItem xs={12}>
+                <Typography display={'inline'} variant={'body1'}>住所：{cus.address}</Typography>
+            </CenteringGridItem>
+            <CenteringGridItem xs={12}>
+                <Button variant={'contained'} color={'secondary'} onClick={customerDelete}>削除</Button>
+            </CenteringGridItem>
         </div>
     )
 };
