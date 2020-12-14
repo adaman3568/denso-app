a8b0d1c04f93377aeae2fd3b58a8e0feb63de291
adaman3568 <adaman3568@gmail.com>
2020-12-14 19:04:46 +0900

    DataContextをクラスコンポーネントで宣言

--- Client/src/Context/DataContext.tsx
+++ Client/src/Context/DataContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, FC, useEffect, useState} from "react";
+import {ComponentProps, createContext, FC, useEffect, useState} from "react";
 import {CarInfo, CommentInfo, CustomerInfo, EmployeeInfo} from "./DataTypeList";
 import React from 'react'
 import {CommentApiDataManager} from "./ApiFunctions/CommentApiDataManager";
@@ -9,64 +9,104 @@ import {CustomerApiDataManager} from "./ApiFunctions/CustomerApiDataManager";
 type DataContextState = {
     isLoading : boolean,
     dataInit : () => void
-    Car : {Data : CarInfo[] , Func: {}}
-    Emp : {Data : EmployeeInfo[] , Func: {}}
-    Customer : {Data : CustomerInfo[] , Func: {}}
-    Comments : {Data : CommentInfo[] , Func: {}}
+    Car : {Data : CarInfo[] ,
+            Func: {
+                GetData : () => Promise<CarInfo[]>,
+                PostData : (data : CarInfo,parentCustomerId : number) => Promise<CarInfo>,
+                PutData : (id : number, data:CarInfo) => Promise<CarInfo>,
+                DeleteData : (id : number) => Promise<void>}}
+    Emp : {Data : EmployeeInfo[] ,
+            Func: {
+                GetData : () => Promise<EmployeeInfo[]>,
+                PostData : (data : EmployeeInfo) => Promise<EmployeeInfo>,
+                PutData : (id : number, data:EmployeeInfo) => Promise<EmployeeInfo>,
+                DeleteData : (id : number) => Promise<void>}}
+    Customer : {Data : CustomerInfo[] ,
+            Func: {
+                GetData : () => Promise<CustomerInfo[]>,
+                PostData : (data : CustomerInfo) => Promise<CustomerInfo>,
+                PutData : (id : number, data:CustomerInfo) => Promise<CustomerInfo>,
+                DeleteData : (id : number) => Promise<void>}}
+    Comments : {Data : CommentInfo[] ,
+            Func: {
+                GetData : () => Promise<CommentInfo[]>,
+                PostData : (data : CommentInfo) => Promise<CommentInfo>,
+                PutData : (id : number, data:CommentInfo) => Promise<CommentInfo>,
+                DeleteData : (id : number) => Promise<void>}}
 }
 
 export const DataContext = createContext<DataContextState>({} as DataContextState)
 
-type dataContextInnerStateType = {
-    CarArray : CarInfo[]
-    EmpArray : EmployeeInfo[],
-    CustomerArray : CustomerInfo[],
-    CommentArray : CommentInfo[],
+interface IState {
+    isLoading : boolean;
+    Car : CarInfo[];
+    Customer : CustomerInfo[];
+    Employee : EmployeeInfo[];
+    Comment : CommentInfo[];
 }
 
-const DataContextProvider : FC = ({children}) => {
+class DataContextProvider extends React.Component<ComponentProps<any>,IState>{
 
-    const [cars,setCars] = useState<CarInfo[]>([]);
-    const [employees,setEmployees] = useState<EmployeeInfo[]>([]);
-    const [customers,setCustomers] = useState<CustomerInfo[]>([]);
-    const [comments,setComments] = useState<CommentInfo[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    private carDataManager : CarApiDataManager;
+    private customerDataManager : CustomerApiDataManager;
+    private employeeDataManager : EmpApiDataManager;
+    private commentDataManager : CommentApiDataManager;
 
-    const commentDataManager = new CommentApiDataManager();
-    const carDataManager = new CarApiDataManager();
-    const customerDataManager = new CustomerApiDataManager();
-    const empDataManager = new EmpApiDataManager();
+    constructor(props : ComponentProps<any>) {
+        super(props);
+        this.setState = this.setState.bind(this);
+        this.state = {Car : [],Customer : [],Employee : [],Comment : [],isLoading : false};
+        this.carDataManager = new CarApiDataManager();
+        this.customerDataManager = new CustomerApiDataManager();
+        this.employeeDataManager = new EmpApiDataManager();
+        this.commentDataManager = new CommentApiDataManager();
+    }
 
-    const getAllData = async () => {
-        commentDataManager.GetData().then(res => setComments(res));
-        carDataManager.GetData().then(res => setCars(res));
-        empDataManager.GetData().then(res => setEmployees(res));
-        customerDataManager.GetData().then(res => setCustomers(res));
-    };
-    // endregion
+    componentDidMount = (): void => {
+        this.dataInit();
+    }
 
-    const dataInit = async () => {
-        setIsLoading(true);
-        await getAllData();
-        setIsLoading(false)
-    };
+    dataInit = () => {
+        this.carDataManager.GetData().then(res => this.setState({...this.state,Car : res}))
+        this.customerDataManager.GetData().then(res => this.setState({...this.state,Customer : res}))
+        this.employeeDataManager.GetData().then(res => this.setState({...this.state,Employee : res}))
+        this.commentDataManager.GetData().then(res => this.setState({...this.state,Comment : res}));
+    }
 
-    return(
-        <DataContext.Provider value={{
+    render = () => {
+        return(
+            <DataContext.Provider value={{
                 Car :
-                    {Data : cars, Func : {}},
+                    {Data : this.state.Car, Func : {
+                            GetData : this.carDataManager.GetData,
+                            PostData : this.carDataManager.PostData,
+                            PutData : this.carDataManager.PutData,
+                            DeleteData : this.carDataManager.DeleteData}},
                 Emp :
-                    {Data : employees , Func : {}},
+                    {Data : this.state.Employee , Func : {
+                            GetData : this.employeeDataManager.GetData,
+                            PostData : this.employeeDataManager.PostData,
+                            PutData : this.employeeDataManager.PutData,
+                            DeleteData : this.employeeDataManager.DeleteData}},
                 Customer :
-                    {Data : customers,Func : {}},
+                    {Data : this.state.Customer,Func : {
+                            GetData : this.customerDataManager.GetData,
+                            PostData : this.customerDataManager.PostData,
+                            PutData : this.customerDataManager.PutData,
+                            DeleteData : this.customerDataManager.DeleteData}},
                 Comments :
-                    {Data :comments,Func : {}},
-            isLoading,
-            dataInit
-        }}>
-            {children}
-        </DataContext.Provider>
-    )
-};
+                    {Data : this.state.Comment,Func : {
+                            GetData : this.commentDataManager.GetData,
+                            PostData : this.commentDataManager.PostData,
+                            PutData : this.commentDataManager.PutData,
+                            DeleteData : this.commentDataManager.DeleteData}},
+                isLoading : this.state.isLoading,
+                dataInit : this.dataInit
+            }}>
+                {this.props.children}
+            </DataContext.Provider>
+        )
+    }
+}
 
 export default DataContextProvider
