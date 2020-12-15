3973c3188957a058321317c8bd5b7944aadab777
adaman3568 <adaman3568@gmail.com>
2020-12-16 08:12:41 +0900

    一旦コミット

--- Client/src/Context/ApiFunctions/EmpApiDataManager.ts
+++ Client/src/Context/ApiFunctions/EmpApiDataManager.ts
@@ -13,18 +13,19 @@ export class EmpApiDataManager implements IApiDataManager<EmployeeInfo>{
         const res = await axios.get(this.path,this.requestHelper.getHeader());
         console.log(res);
         return res.data as EmployeeInfo[];
-    }
+    };
+
     DeleteData = async (id: number): Promise<void> => {
         await axios.delete(`${this.path}/${id}` ,this.requestHelper.getHeader());
-    }
+    };
 
     PostData = async (data: EmployeeInfo): Promise<EmployeeInfo> => {
         const res = await axios.post(`${this.path}`,data ,this.requestHelper.getHeader());
         return res.data as EmployeeInfo
-    }
+    };
 
     PutData = async (id: number, data: EmployeeInfo): Promise<EmployeeInfo> => {
         const res = await axios.put(`${this.path}/${id}`,data ,this.requestHelper.getHeader());
         return res.data as EmployeeInfo
-    }
+    };
 }
