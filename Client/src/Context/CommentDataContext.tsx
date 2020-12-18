ab04643d65228f3eccca9076b96ed6d6386fc59a
浩 林山 <adaman3568@gmail.com>
2020-12-18 16:38:30 +0900

    CommentDataContextを追加

---
+++ Client/src/Context/CommentDataContext.tsx
@@ -0,0 +1,74 @@
+import {CarInfo, CommentInfo, CustomerInfo, EmployeeInfo} from "./DataTypeList";
+import {createContext, useEffect, useReducer, useState} from "react";
+import CommentReducer, {
+    CommentDeleteActionCreator,
+    CommentInsertActionCreator,
+    CommentsReadActionCreator,
+    CommentUpdateActionCreator
+} from "../Reducers/CommentReducer";
+import {CommentApiDataManager} from "./ApiFunctions/CommentApiDataManager";
+import React from "react";
+
+type DataContextState = {
+    isLoading : boolean,
+    Data : CommentInfo[] ,
+    Func: {
+            GetData : () => void,
+            PostData : (data : CommentInfo) => void,
+            PutData : (id : number, data:CommentInfo) => void,
+            DeleteData : (id : number) => void}
+}
+
+export const CommentDataContext = createContext<DataContextState>({} as DataContextState);
+export const CommentDataContextProvider : React.FC = ({children}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [state,dispatch] = useReducer(CommentReducer,[]);
+
+    useEffect(() => {
+        getData();
+    },[])
+
+    // region Valueで渡すメソッド
+    const getData = () => {
+        new CommentApiDataManager().GetData().then(res => {
+            dispatch(CommentsReadActionCreator(res))
+            setIsLoading(false)
+        }).catch(err => console.log(err));
+    }
+
+    const insertData = (data : CommentInfo) => {
+        new CommentApiDataManager()
+            .PostData(data)
+            .then(res => dispatch(CommentInsertActionCreator(res)))
+            .catch(err => console.log(err));
+    }
+    const updateData = (id : number,data : CommentInfo) => {
+        new CommentApiDataManager()
+            .PutData(id,data)
+            .then(res => dispatch(CommentUpdateActionCreator(id,res)))
+            .catch(err => console.log(err));
+    }
+    const deleteData = (id : number) => {
+        new CommentApiDataManager()
+            .DeleteData(id)
+            .then(res => dispatch(CommentDeleteActionCreator(id)))
+            .catch(err => console.log(err));
+    }
+    // endregion
+
+    return (
+        <CommentDataContext.Provider value={{
+            isLoading : isLoading,
+            Data : state,
+            Func : {
+                GetData : getData,
+                PostData : insertData,
+                PutData : updateData,
+                DeleteData : deleteData
+            }
+        }}>
+            {children}
+        </CommentDataContext.Provider>
+    )
+
+}
