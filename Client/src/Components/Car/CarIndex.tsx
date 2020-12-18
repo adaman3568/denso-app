002595de97152d97434a72788481930ac0b45e38
浩 林山 <adaman3568@gmail.com>
2020-12-18 17:21:08 +0900

    CarIndexでCarDataContextを使えるように

--- Client/src/Components/Car/CarIndex.tsx
+++ Client/src/Components/Car/CarIndex.tsx
@@ -1,23 +1,18 @@
 import React, {FC, useContext, useEffect, useState} from 'react';
 import Title from "../Common/Title";
 import CarItem from "./CarItem";
-import {CarInfo, CommentInfo} from "../../Context/DataTypeList";
+import {CarInfo} from "../../Context/DataTypeList";
 import useEditModal from "../../CustomHooks/useEditModal";
 import {CarEdit} from "./CarCreateEdit";
 import useDeleteModal from "../../CustomHooks/useDeleteModal";
 import {DeleteCar} from "./CarDelete";
-import {DataContext} from "../../Context/DataContext";
 import Loading from "../Common/Loading";
+import {CarDataContext} from "../../Context/CarDataContext";
 
 const CarIndex : FC = () => {
     const carEditModal = useEditModal<CarInfo>(CarEdit);
     const carDeleteModal = useDeleteModal<CarInfo>(DeleteCar)
-    const {Car,isLoading} = useContext(DataContext)
-
-    const [carItems,setCarItems] = useState<CarInfo[]>(Car.Data);
-    useEffect(() => {
-        setCarItems(Car.Data)
-    },[Car.Data])
+    const {Data,isLoading} = useContext(CarDataContext)
 
     if(isLoading)
         return <Loading/>
@@ -25,7 +20,7 @@ const CarIndex : FC = () => {
     return (
         <div>
             <Title>This is CarIndex page.</Title>
-            {carItems.map(car => <CarItem key={car.id} Data={car} DeleteModalOpen={carDeleteModal.OpenModal} EditModalOpen={carEditModal.OpenModal}/>)}
+            {Data.map(car => <CarItem key={car.id} Data={car} DeleteModalOpen={carDeleteModal.OpenModal} EditModalOpen={carEditModal.OpenModal}/>)}
             {carEditModal.Modal()}
             {carDeleteModal.Modal()}
         </div>
