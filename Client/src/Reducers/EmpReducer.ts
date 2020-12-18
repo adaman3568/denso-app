dc2ed6833f8af9404fe5e0431c89186d3ad66e6c
浩 林山 <adaman3568@gmail.com>
2020-12-18 17:36:12 +0900

    EmoloyeeReducerを作成

--- Client/src/Reducers/EmpReducer.ts
+++ Client/src/Reducers/EmpReducer.ts
@@ -1,25 +1,52 @@
-export const decoi = {}
-// import {EmployeeInfo} from "../Context/DataTypeList";
-// import React from "react";
-//
-// const EmpInitialState : EmployeeInfo[] = [];
-//
-// const SetAllEmployeeAction = 'SetAllEmployeeAction' as const;
-//
-// export const SetDisplayEmpActionCreator = (data : EmployeeInfo[]) => ({
-//     type : SetAllEmployeeAction,
-//     payload : data,
-//     });
-//
-// export type EmployeeActions = ReturnType<typeof SetDisplayEmpActionCreator>
-//
-// const EmpReducer : React.Reducer<EmployeeInfo[],EmployeeActions> = (status = EmpInitialState , action) : EmployeeInfo[] => {
-//     switch (action.type) {
-//         case SetAllEmployeeAction:
-//             return action.payload;
-//         default:
-//             return status;
-//     }
-// };
-//
-// export default EmpReducer;
+import React from "react";
+import {EmployeeInfo} from "../Context/DataTypeList";
+export const EmpInitialState : EmployeeInfo[] = [];
+
+export const EmpReadAllAction = 'eraa' as const;
+export const EmpUpdateAction = 'eua' as const;
+export const EmpInsertAction = 'eia' as const;
+export const EmpDeleteAction = 'eda' as const;
+
+export const EmpReadAllActionCreator = (data : EmployeeInfo[]) => ({
+    type : EmpReadAllAction,
+    payload : {data},
+});
+export const EmpUpdateActionCreator = (id : number,data : EmployeeInfo) => ({
+    type : EmpUpdateAction,
+    payload : {id,data},
+});
+export const EmpInsertActionCreator = (data : EmployeeInfo) => ({
+    type : EmpInsertAction,
+    payload : {data},
+});
+export const EmpDeleteActionCreator = (id : number) => ({
+    type : EmpDeleteAction,
+    payload : {id},
+});
+
+
+export type CustomerActions =
+    ReturnType<typeof EmpReadAllActionCreator> |
+    ReturnType<typeof EmpInsertActionCreator> |
+    ReturnType<typeof EmpUpdateActionCreator> |
+    ReturnType<typeof EmpDeleteActionCreator>
+
+const EmployeeReducer : React.Reducer<EmployeeInfo[],CustomerActions> = (status = EmpInitialState , action) : EmployeeInfo[] => {
+    switch (action.type) {
+        case EmpReadAllAction:
+            return action.payload.data;
+        case EmpDeleteAction:
+            return status.filter(item => item.id !== action.payload.id)
+        case EmpInsertAction:
+            return [action.payload.data,...status]
+        case EmpUpdateAction:
+            const filterdData = status.filter(item => item.id !== action.payload.id);
+            return [action.payload.data,...filterdData]
+        default:
+            return status;
+    }
+};
+
+export default EmployeeReducer;
+
+
