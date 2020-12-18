f3c29871c5bc49ecefd010867e5c541986603c53
浩 林山 <adaman3568@gmail.com>
2020-12-18 17:08:51 +0900

    CustomerIndexでCustomerDataContextを使うように

--- Client/src/Components/Customer/CustomerIndex.tsx
+++ Client/src/Components/Customer/CustomerIndex.tsx
@@ -2,26 +2,21 @@ import React, {FC, useContext, useEffect, useState} from 'react';
 import CustomerItem from "./CustomerItem";
 import Title from "../Common/Title";
 import {Button} from "@material-ui/core";
-import {CommentInfo, CustomerInfo} from "../../Context/DataTypeList";
+import {CustomerInfo} from "../../Context/DataTypeList";
 import useDeleteModal from "../../CustomHooks/useDeleteModal";
 import useEditModal from "../../CustomHooks/useEditModal";
 import useInsertModal from "../../CustomHooks/useInsertModal";
 import {DeleteCustomer} from "./ComfirmDeleteCustomer";
 import {EditCustomer, InsertCustomer} from "./CustomerCreateEdit";
-import {DataContext} from "../../Context/DataContext";
 import Loading from "../Common/Loading";
+import {CustomerDataContext} from "../../Context/CustomerDataContext";
 
 const CustomerIndex : FC = () => {
 
     const editModal = useEditModal<CustomerInfo>(EditCustomer);
     const insertModal = useInsertModal(InsertCustomer);
     const deleteModal = useDeleteModal<CustomerInfo>(DeleteCustomer);
-    const {Customer,isLoading} = useContext(DataContext)
-    const [customer,setCustomer] = useState<CustomerInfo[]>(Customer.Data);
-
-    useEffect(() => {
-        setCustomer(Customer.Data)
-    },[Customer.Data])
+    const {Data,Func,isLoading} = useContext(CustomerDataContext)
 
     if(isLoading)
         return <Loading/>
@@ -30,7 +25,7 @@ const CustomerIndex : FC = () => {
         <div>
             <Title>this is CustomerIndex page.</Title>
             <Button size={'small'} color={"primary"} variant="contained" onClick={insertModal.OpenModal}>顧客追加</Button>
-            {customer.map((cu,index) => <CustomerItem key={index} Data={cu} EditModalOpen={editModal.OpenModal} DeleteModalOpen={deleteModal.OpenModal}/> )}
+            {Data?.map((cu,index) => <CustomerItem key={index} Data={cu} EditModalOpen={editModal.OpenModal} DeleteModalOpen={deleteModal.OpenModal}/> )}
 
             {editModal.Modal()}
             {insertModal.Modal()}
