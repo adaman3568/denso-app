8c07161ea7b8a05b2cf65264c7112159f39381bb
浩 林山 <adaman3568@gmail.com>
2020-12-18 15:28:22 +0900

    コメントReducerを作成

--- Client/src/Reducers/CommentReducer.ts
+++ Client/src/Reducers/CommentReducer.ts
@@ -1,25 +1,52 @@
-export const decoi = {}
-// import {CommentInfo} from "../Context/DataTypeList";
-// import React from "react";
-//
-// export const EmpInitialState : CommentInfo[] = [];
-//
-// const SetAllCommentAction = 'SetAllCommentAction' as const;
-//
-// export const SetDisplayCommentActionCreator = (data : CommentInfo[]) => ({
-//     type : SetAllCommentAction,
-//     payload : data,
-// });
-//
-// export type CommentActions = ReturnType<typeof SetDisplayCommentActionCreator>
-//
-// const CommentReducer : React.Reducer<CommentInfo[],CommentActions> = (status = EmpInitialState , action) : CommentInfo[] => {
-//     switch (action.type) {
-//         case SetAllCommentAction:
-//             return action.payload;
-//         default:
-//             return status;
-//     }
-// };
-//
-// export default CommentReducer;
+import {CommentInfo} from "../Context/DataTypeList";
+import React from "react";
+
+export const CommentsInitialState : CommentInfo[] = [];
+
+const CommentReadAllAction = 'CommentReadAllAction' as const;
+const CommentUpdate = 'CommentUpdateAction' as const;
+const CommentInsert = 'CommentInsertAction' as const;
+const CommentDelete = 'CommentDeleteAction' as const;
+export const CommentsReadActionCreator = (data : CommentInfo[]) => ({
+    type : CommentReadAllAction,
+    payload : {Data : data},
+});
+
+export const CommentUpdateActionCreator = (id : number,updatedData : CommentInfo) => ({
+    type : CommentUpdate,
+    payload : {id :id,Data : updatedData},
+});
+
+export const CommentInsertActionCreator = (newData : CommentInfo) => ({
+    type : CommentInsert,
+    payload : {Data : newData},
+});
+
+export const CommentDeleteActionCreator = (deleteId : number) => ({
+    type : CommentDelete,
+    payload : {id : deleteId},
+});
+
+export type CommentActions =
+    ReturnType<typeof CommentsReadActionCreator> |
+    ReturnType<typeof CommentInsertActionCreator> |
+    ReturnType<typeof CommentUpdateActionCreator> |
+    ReturnType<typeof CommentDeleteActionCreator>
+
+const CommentReducer : React.Reducer<CommentInfo[],CommentActions> = (status = CommentsInitialState , action : CommentActions) : CommentInfo[] => {
+    switch (action.type) {
+        case CommentReadAllAction:
+            return action.payload.Data;
+        case CommentInsert:
+            return [action.payload.Data,...status]
+        case CommentDelete:
+            return status.filter(item => item.id !== action.payload.id);
+        case CommentUpdate:
+            const filterdItems = status.filter(item => item.id !== action.payload.id);
+            return [action.payload.Data,...filterdItems];
+        default:
+            return status;
+    }
+};
+
+export default CommentReducer;
